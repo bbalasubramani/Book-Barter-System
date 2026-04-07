5bd5747d74d3bfd67275d5627ee431e0084cd219
Balasubramani B <bala026b@gmail.com>
2026-04-07 21:17:57 +0530

    Simplify MongoDB connection code

--- server/app.js
+++ server/app.js
@@ -21,10 +21,8 @@ app.use(cors({
 }));
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => console.log('MongoDB connected'));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'));
 
 app.use('/api/auth', authRoutes);
 app.use('/api/books', bookRoutes);
